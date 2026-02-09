021055629230c07da3b0ff4d2f535b52345eeb94
kohei <bhkbb0551@gmail.com>
2026-02-09 10:37:15 +0900

    cloudrun作成

---
+++ projects/szkou-mcp-practice/cloud_run.tf
@@ -0,0 +1,41 @@
+resource "google_cloud_run_v2_service" "mcp_on_cloudrun_app" {
+  name                 = "mcp-on-cloudrun-app"
+  location             = "asia-northeast1"
+  project              = "szkou-mcp-practice"
+  deletion_protection  = false
+  ingress              = "INGRESS_TRAFFIC_ALL"
+  invoker_iam_disabled = false
+
+  scaling {
+    min_instance_count    = 0
+    manual_instance_count = 0
+  }
+
+  template {
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1
+    }
+
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        limits = {
+          cpu = "1"
+          memory = "1Gi"
+        }
+
+      }
+    }
+
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image,
+      template[0].labels,
+      client,
+      client_version,
+    ]
+  }
+}
